15772a964a0a48c9ee7c458f05a4b4c7909a63c5
yashodipthakare <ganuthakare99@gmail.com>
2025-06-17 19:02:48 +0530

    Spring Starting

---
+++ SpringBoot/Productlist/src/test/java/com/yash/Productlist/ProductlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.Productlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
