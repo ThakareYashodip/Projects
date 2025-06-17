c1c692e23db6dbfdfe097d343a08324ac3574230
yashodipthakare <ganuthakare99@gmail.com>
2025-06-17 20:15:29 +0530

    ..

--- SpringBoot/Productlist/src/main/java/com/yash/Productlist/MyComponent.java
+++ SpringBoot/Productlist/src/main/java/com/yash/Productlist/MyComponent.java
@@ -3,14 +3,16 @@ package com.yash.Productlist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MyComponent {
 
     public MyComponent(){
         System.out.println("Hello SpringBoot");
     }
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
         return  "Hello Yash !!!";
     }
