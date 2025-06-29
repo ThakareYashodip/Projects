95a42bf898760295abaa75d41189661bee7d9758
yashodipthakare <ganuthakare99@gmail.com>
2025-06-29 17:02:47 +0530

    updated entity classe

---
+++ SpringBoot/UrbanServe/src/main/java/com/example/UrbanServe/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.UrbanServe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    public enum Role {
+        RESIDENT,
+        ADMIN
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = true)
+    private String flatNumber;
+}
