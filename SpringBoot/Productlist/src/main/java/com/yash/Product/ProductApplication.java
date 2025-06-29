d4bb7fabf8dd89a07b55ca8b15ab94b7f0c34c32
yashodipthakare <ganuthakare99@gmail.com>
2025-06-29 17:00:31 +0530

    Updated DTO(Data Transfer Objetc) files

--- SpringBoot/Productlist/src/main/java/com/yash/Product/ProductApplication.java
+++ SpringBoot/Productlist/src/main/java/com/yash/Product/ProductApplication.java
@@ -1,8 +1,28 @@
 package com.yash.Product;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Product Service REST API Documentation",
+				description = "Product Service API",
+				version = "v1",
+				contact = @Contact(
+						name = "Yashodip Raju Thakare",
+						email = "ganuthakare99@gmail.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "sharepoint URL Product Service API",
+				url = "example.com"
+		)
+)
 
 @SpringBootApplication
 public class ProductApplication {
