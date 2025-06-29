cacb052aaac53a40793e3e036aad7e0a5b8c7028
yashodipthakare <ganuthakare99@gmail.com>
2025-06-29 17:24:16 +0530

    Created authentication REST API , to register user data and login user .

---
+++ SpringBoot/UrbanServe/src/main/java/com/example/UrbanServe/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.UrbanServe.controller;
+
+import com.example.UrbanServe.dto.LoginRequestDTO;
+import com.example.UrbanServe.dto.RegisterRequestDTO;
+import com.example.UrbanServe.dto.UserDTO;
+import com.example.UrbanServe.entity.User;
+import com.example.UrbanServe.mapper.UserMapper;
+import com.example.UrbanServe.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> registerUser(@RequestBody RegisterRequestDTO dto){
+        User newUser = userService.registerUser(dto);
+        return ResponseEntity.ok(UserMapper.userEntityToDTO(newUser));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> loginUser(@RequestBody LoginRequestDTO dto){
+        User user = userService.authenticateUser(dto.getEmail(), dto.getPassword());
+        return ResponseEntity.ok(UserMapper.userEntityToDTO(user));
+    }
+
+}
