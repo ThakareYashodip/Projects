2233b2c3b0773f56a6fe95a1f97f9f00e67d6b36
yashodipthakare <ganuthakare99@gmail.com>
2025-07-30 16:19:00 +0530

    Open Voice Latest Project Integrations

---
+++ SpringBoot/OpenVoice/src/main/java/OpenVoice/OpenVoice/entity/Media.java
@@ -0,0 +1,4 @@
+package OpenVoice.OpenVoice.entity;
+
+public class Media {
+}
--- SpringBoot/OpenVoice/src/main/java/OpenVoice/OpenVoice/entity/User.java
+++ SpringBoot/OpenVoice/src/main/java/OpenVoice/OpenVoice/entity/User.java
@@ -1,5 +1,26 @@
 package OpenVoice.OpenVoice.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Users")
+@Data // Getter , Setter , ToString , RequiredArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "User";
+
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+//    private List<Report> reports;
 }
