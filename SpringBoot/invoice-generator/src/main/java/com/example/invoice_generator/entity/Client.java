44fa19c92e5a4d709e64fd46aaff41337846e601
yashodipthakare <ganuthakare99@gmail.com>
2025-08-19 16:06:14 +0530

    Author Service and controllers and entity everything is done .

--- SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/entity/Client.java
+++ SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/entity/Client.java
@@ -3,6 +3,9 @@ package com.example.invoice_generator.entity;
 import com.example.invoice_generator.entity.BaseEntity;
 import com.example.invoice_generator.entity.Invoice;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -14,9 +17,18 @@ import java.util.List;
 @Builder
 public class Client extends BaseEntity {
 
+    @NotBlank
+    @Size(min = 3,max = 30 , message = "Client name must required !")
     private String name;
+
+    @Email
     private String email;
+
+    @Size(min = 10,max = 10,message = "Phone number must be 10 digit . ")
     private String phone;
+
+    @NotBlank
+    @Size(min = 3, max = 100, message = "Address must be between 3 to 100 characters . ")
     private String address;
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
