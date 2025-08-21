37ae7b4c210afe1acd66566b8cb5eeeeb423e225
yashodipthakare <ganuthakare99@gmail.com>
2025-08-21 23:48:38 +0530

    New Project For Invoice Generation . Useful for small business and marketplaces. DTO Creation has done. (Basic)

--- SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/dto/InvoiceDTO.java
+++ SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/dto/InvoiceDTO.java
@@ -1,16 +1,20 @@
 package com.example.invoice_generator.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@NoArgsConstructor @AllArgsConstructor
 @Data
 public class InvoiceDTO {
     private Long id ;
     private String invoicenumber;
     private LocalDate invoiceDate;
     private Long clientId;
-    private List<InvoiceDTO> items;
+    private List<InvoiceItemDTO> items;
     private double total;
 }
