26ca610c7db4185fa815d27c9f10731205f2d1f4
yashodipthakare <ganuthakare99@gmail.com>
2025-08-25 16:53:24 +0530

    New Project For Invoice Generation . Useful for small business and marketplaces. DTO Creation has done. (Basic)

--- SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/dto/InvoiceItemDTO.java
+++ SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/dto/InvoiceItemDTO.java
@@ -1,4 +1,15 @@
 package com.example.invoice_generator.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor
+@Data
 public class InvoiceItemDTO {
+    private Long id;
+    private String description;
+    private int quantity;
+    private double price;
+
 }
--- SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/entity/InvoiceItem.java
+++ SpringBoot/invoice-generator/src/main/java/com/example/invoice_generator/entity/InvoiceItem.java
@@ -3,6 +3,8 @@ package com.example.invoice_generator.entity;
 import com.example.invoice_generator.entity.BaseEntity;
 import com.example.invoice_generator.entity.Invoice;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -12,6 +14,8 @@ import lombok.*;
 @Builder
 public class InvoiceItem extends BaseEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 100 , message = "Description should be upto 100 words.")
     private String description;
     private Integer quantity;
     private Double unitPrice;
